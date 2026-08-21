e2572660ad42cc8d4db069854be3c0482a17884b
Lucas Inga <lucas.inga@tecsup.edu.pe>
2026-08-21 15:13:30 -0500

    Se indago lo que se pedia ,se añadio un Button con la etiqueta de 'Presioname', cambiamos los colores del tema de la app que estamos realizando.

--- app/src/main/java/com/inga/laboratorio01/MainActivity.kt
+++ app/src/main/java/com/inga/laboratorio01/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.inga.laboratorio01.ui.theme.Laboratorio01Theme
+import androidx.compose.material3.Button
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +39,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Text(text = "Hola, $name", fontSize = 24.sp)
         Text(text = "Curso: Programacion en Moviles")
         Text(text = "Docente: Juan Leon Suiyon")
+
+        Button(onClick = {}) {
+            Text(text = "Presioname")
+        }
     }
 }
 
--- app/src/main/java/com/inga/laboratorio01/ui/theme/Color.kt
+++ app/src/main/java/com/inga/laboratorio01/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.inga.laboratorio01.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFF12317E)
+val PurpleGrey80 = Color(0xFF0A594D)
+val Pink80 = Color(0xFFB8EFDF)
 
-val Purple40 = Color(0xFF6650a4)
+val Purple40 = Color(0xFF310606)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
