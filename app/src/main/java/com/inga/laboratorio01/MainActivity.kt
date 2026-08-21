138f18a9af7d114c14b0818d09dffcb9b526096f
Lucas Inga <lucas.inga@tecsup.edu.pe>
2026-08-21 14:23:18 -0500

    Initial commit

---
+++ app/src/main/java/com/inga/laboratorio01/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.inga.laboratorio01
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.inga.laboratorio01.ui.theme.Laboratorio01Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laboratorio01Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hola, soy Lucas Inga!", fontSize = 24.sp)
+        Text(text = "Curso: Programacion en Moviles")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laboratorio01Theme {
+        Greeting("Lucas Inga")
+    }
+}
